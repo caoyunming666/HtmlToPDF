2677d4537b83ddffeb2293bc04a2299035f1d2c3
yunming cao <caoyunming666@sina.com>
2018-11-08 15:57:39 +0800

    上传一个代码文件：记录了如何在程序内调用另一个程序(比如 调用cmd命令窗口...)

--- HtmlToPDF_Demo/Controllers/HomeController.cs
+++ HtmlToPDF_Demo/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
 using iTextSharp.tool.xml;
+using Pechkin;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,6 +22,23 @@ namespace HtmlToPDF_Demo.Controllers
             return View();
         }
 
+
+        public ActionResult DownloadPdf2()
+        {
+            string url = "http://www.baidu.com/";
+
+            using (IPechkin pechkin = Factory.Create(new GlobalConfig()))
+            {
+                ObjectConfig oc = new ObjectConfig();
+                oc.SetPrintBackground(true);
+                oc.SetLoadImages(true);
+                oc.SetScreenMediaType(true);
+                oc.SetPageUri(url);
+                byte[] pdf = pechkin.Convert(oc);
+                return File(pdf, "application/pdf", "ExportPdf.pdf");
+            }
+        }
+
         /// <summary>
         /// 執行此Url，下載PDF檔案
         /// </summary>
@@ -27,8 +46,33 @@ namespace HtmlToPDF_Demo.Controllers
         public ActionResult DownloadPdf()
         {
             string htmlText = GetViewHtml(ControllerContext, "Index", null);
-            byte[] pdfFile = this.ConvertHtmlTextToPDF(htmlText);
-            return new BinaryContentResult(pdfFile, "application/pdf");
+
+            if (string.IsNullOrEmpty(htmlText))
+            {
+                return null;
+            }
+
+            //string compHtml = "<html><head><style>@font-face{font-family: \"宋体\";src:url(\"C:\\Windows\\Fonts\\simsun.ttf\");}</style></head><body>";
+
+            string compHtml = "<html><head></head><body>";
+
+
+            //避免當htmlText無任何html tag標籤的純文字時，轉PDF時會掛掉，所以一律加上<p>標籤
+            compHtml += "<p>" + htmlText + "</p></body></html>";
+            string tempPostContent = getImage(compHtml);
+
+            #region 字符串，字节数组相互转换
+            //byte[] data = Encoding.BigEndianUnicode.GetBytes(tempPostContent);//来取得字节
+            //string str = Encoding.BigEndianUnicode.GetString(data);//来取得字符.
+            //byte[] data = Encoding.UTF8.GetBytes(tempPostContent);//字串轉成byte[]
+            //string str = Encoding.Default.GetString(data); 
+            #endregion
+
+            HtmlTextConvertToPdf(tempPostContent);
+
+            //byte[] pdfFile = this.ConvertHtmlTextToPDF(htmlText);
+            //return new BinaryContentResult(pdfFile, "application/pdf");
+            return Json(new { }, JsonRequestBehavior.AllowGet);
         }
 
         private byte[] ConvertHtmlTextToPDF(string htmlText)
@@ -144,6 +188,210 @@ namespace HtmlToPDF_Demo.Controllers
                     throw new ArgumentException("无法获取视图生成的HTML代码");
                 }
                 return sw.GetStringBuilder().ToString();
+            }
+        }
+
+
+
+
+
+        /// <summary>
+        /// HTML文本内容转换为PDF
+        /// </summary>
+        /// <param name="strHtml">HTML文本内容</param>
+        /// <param name="savePath">PDF文件保存的路径</param>
+        /// <returns></returns>
+        public bool HtmlTextConvertToPdf(string strHtml)
+        {
+            string appRootPath = AppDomain.CurrentDomain.BaseDirectory;
+            var fullpath = appRootPath + "/ConvertToPdf";
+            if (!Directory.Exists(fullpath))
+            {
+                Directory.CreateDirectory(fullpath);
+            }
+            string fileName = fullpath + "/" + DateTime.Now.ToString("yyyyMMddHHmmss") + new Random().Next(1000, 10000) + ".pdf";
+
+            bool flag = false;
+            try
+            {
+                string htmlPath = HtmlTextConvertFile(strHtml);
+
+                flag = HtmlConvertToPdf(htmlPath, fileName);
+                //System.IO.File.Delete(htmlPath);
+            }
+            catch
+            {
+                flag = false;
+            }
+            return flag;
+        }
+
+
+        /// <summary>
+        /// HTML转换为PDF
+        /// </summary>
+        /// <param name="htmlPath">可以是本地路径，也可以是网络地址</param>
+        /// <param name="savePath">PDF文件保存的路径</param>
+        /// <returns></returns>
+        public bool HtmlConvertToPdf(string htmlPath, string savePath)
+        {
+            bool flag = false;
+            CheckFilePath(savePath);
+
+            ///这个路径为程序集的目录，因为我把应用程序 wkhtmltopdf.exe 放在了程序集同一个目录下
+            string exePath = @"D:\ITtools\wkhtmltopdf\bin\wkhtmltopdf.exe";
+            if (!System.IO.File.Exists(exePath))
+            {
+                throw new Exception("No application wkhtmltopdf.exe was found.");
+            }
+
+            try
+            {
+                /* 
+                 * 在程序中调用另一个程序
+                 *      调用cmd窗口也是这样写
+                 */
+                ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                processStartInfo.FileName = exePath;
+                processStartInfo.WorkingDirectory = Path.GetDirectoryName(exePath);
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.CreateNoWindow = true;
+                processStartInfo.RedirectStandardInput = true;
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.RedirectStandardError = true;
+                processStartInfo.Arguments = GetArguments(htmlPath, savePath);
+
+                Process process = new Process();
+                process.StartInfo = processStartInfo;
+                process.Start();
+                process.WaitForExit();
+
+                #region 用于查看是否返回错误信息
+                //StreamReader srone = process.StandardError;
+                //StreamReader srtwo = process.StandardOutput;
+                //string ss1 = srone.ReadToEnd();
+                //string ss2 = srtwo.ReadToEnd();
+                //srone.Close();
+                //srone.Dispose();
+                //srtwo.Close();
+                //srtwo.Dispose(); 
+                #endregion
+
+                process.Close();
+                process.Dispose();
+
+                flag = true;
+            }
+            catch
+            {
+                flag = false;
+            }
+            return flag;
+        }
+
+
+        /// <summary>
+        /// 获取命令行参数
+        /// </summary>
+        /// <param name="htmlPath"></param>
+        /// <param name="savePath"></param>
+        /// <returns></returns>
+        private string GetArguments(string htmlPath, string savePath)
+        {
+            if (string.IsNullOrEmpty(htmlPath))
+            {
+                throw new Exception("HTML local path or network address can not be empty.");
+            }
+
+            if (string.IsNullOrEmpty(savePath))
+            {
+                throw new Exception("The path saved by the PDF document can not be empty.");
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            //stringBuilder.Append(" --page-height 100 ");        //页面高度100mm
+            //stringBuilder.Append(" --page-width 100 ");         //页面宽度100mm
+            //stringBuilder.Append(" --header-center 我是页眉 ");  //设置居中显示页眉
+            //stringBuilder.Append(" --header-line ");         //页眉和内容之间显示一条直线
+            //stringBuilder.Append(" --footer-center \"Page [page] of [topage]\" ");    //设置居中显示页脚
+            //stringBuilder.Append(" --footer-line ");       //页脚和内容之间显示一条直线
+            stringBuilder.Append(" " + htmlPath + " ");       //本地 HTML 的文件路径或网页 HTML 的URL地址
+            stringBuilder.Append(" " + savePath + " ");       //生成的 PDF 文档的保存路径
+            return stringBuilder.ToString();
+        }
+
+
+        /// <summary>
+        /// 验证保存路径
+        /// </summary>
+        /// <param name="savePath"></param>
+        private void CheckFilePath(string savePath)
+        {
+            string ext = string.Empty;
+            string path = string.Empty;
+            string fileName = string.Empty;
+
+            ext = Path.GetExtension(savePath);
+            if (string.IsNullOrEmpty(ext) || ext.ToLower() != ".pdf")
+            {
+                throw new Exception("Extension error:This method is used to generate PDF files.");
+            }
+
+            fileName = Path.GetFileName(savePath);
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new Exception("File name is empty.");
+            }
+
+            try
+            {
+                path = savePath.Substring(0, savePath.IndexOf(fileName));
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch
+            {
+                throw new Exception("The file path does not exist.");
+            }
+        }
+
+        /// <summary>
+        /// HTML文本内容转HTML文件
+        /// </summary>
+        /// <param name="strHtml">HTML文本内容</param>
+        /// <returns>HTML文件的路径</returns>
+        public string HtmlTextConvertFile(string strHtml)
+        {
+            if (string.IsNullOrEmpty(strHtml))
+            {
+                throw new Exception("HTML text content cannot be empty.");
+            }
+
+            try
+            {
+                string appRootPath = AppDomain.CurrentDomain.BaseDirectory;
+                var fullpath = appRootPath + "/HtmlTemplate";
+                if (!Directory.Exists(fullpath))
+                {
+                    Directory.CreateDirectory(fullpath);
+                }
+                string fileName = fullpath + "/" + DateTime.Now.ToString("yyyyMMddHHmmss") + new Random().Next(1000, 10000) + ".html";
+                FileStream fileStream = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                StreamWriter streamWriter = new StreamWriter(fileStream, Encoding.Default);
+                streamWriter.Write(strHtml);
+                streamWriter.Flush();
+
+                streamWriter.Close();
+                streamWriter.Dispose();
+                fileStream.Close();
+                fileStream.Dispose();
+                return fileName;
+            }
+            catch
+            {
+                throw new Exception("HTML text content error.");
             }
         }
     }
